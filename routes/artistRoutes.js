5b813822973fa3feed72b43ded60949522690236
freshartclub <devops@freshartclub.com>
2024-09-30 10:21:15 +0530

    Remove the unneccsary code

--- routes/artistRoutes.js
+++ routes/artistRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const BecomeArtist = require("../models/becomeArtistModel");
 const { login, becomeArtist } = require("../controller/artistController");
 
 router.post("/login", login);
