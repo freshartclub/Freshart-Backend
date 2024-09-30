39ac1a2713284fd7bddc333979185696d20991c1
freshartclub <devops@freshartclub.com>
2024-09-30 18:53:03 +0530

    changes added

--- functions/upload.js
+++ functions/upload.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
+
 		if (file?.fieldname === "uploadDocs") {
 			cb(null, "./public/uploads/documents");
 		} else {
@@ -15,25 +16,10 @@ const storage = multer.diskStorage({
 	},
 
 	filename: function (req, file, cb) {
-		const fileExtension = file.originalname.substr(
-			file.originalname.lastIndexOf(".") + 1,
-			file.originalname.length
-		);
-		let data;
-		if (
-			[
-				"profileImage",
-				"additionalImage",
-				"inProcessImage",
-				"uploadDocs",
-				"additionalVideo",
-				"mainVideo",
-				"insigniaImage",
-			].includes(file?.fieldname)
-		) {
+		const fileExtension = file.originalname.substr(file.originalname.lastIndexOf(".") + 1, file.originalname.length);
+		let data = req?.user?._id
+		if (["profileImage", "additionalImage", "inProcessImage", "uploadDocs", "additionalVideo", "mainVideo", "insigniaImage"].includes(file?.fieldname)) {
 			data = mongoose.Types.ObjectId();
-		} else {
-			data = req?.user?._id;
 		}
 		cb(null, `${data}.${fileExtension}`);
 	},
@@ -41,27 +27,19 @@ const storage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
 	if (file?.fieldname === "uploadDocs") {
-		const fileExtension = file.originalname.substr(
-			file.originalname.lastIndexOf(".") + 1,
-			file.originalname.length
-		);
+		const fileExtension = file.originalname.substr(file.originalname.lastIndexOf(".") + 1, file.originalname.length);
 
-		if (
-			["docx", "xlsx"].includes(fileExtension) ||
-			file.mimetype === "application/pdf"
-		) {
+		if (["docx", "xlsx"].includes(fileExtension) || file.mimetype === "application/pdf") {
 			return cb(null, true);
 		}
-	} else if (["additionalVideo", "mainVideo"].includes(file?.fieldname)) {
+	}
+
+	if (["additionalVideo", "mainVideo"].includes(file?.fieldname)) {
 		if (file.mimetype === "video/mp4") {
 			return cb(null, true);
 		}
 	} else {
-		if (
-			file.mimetype === "image/jpeg" ||
-			file.mimetype === "image/jpg" ||
-			file.mimetype === "image/png"
-		) {
+		if (['image/jpeg', 'image/jpg', 'image/png'].includes(file.mimetype)) {
 			return cb(null, true);
 		}
 	}
