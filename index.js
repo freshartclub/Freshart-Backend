77f0b499afdb6183dac5e5a1f0d4fcd9d4eb46f4
freshartclub <devops@freshartclub.com>
2025-01-17 16:12:20 +0530

    chnges

--- index.js
+++ index.js
@@ -14,18 +14,7 @@ const allowedOrigins = [
   "https://test.freshartclub.com",
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: false,
-  })
-);
+app.use(cors({ origin: allowedOrigins }));
 
 const session = require("express-session");
 const port = process.env.PORT || 4000;
